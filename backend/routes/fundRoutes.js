a553120bbcd882301c5e4bf26b1e72926f46a985
meghana.n <meghana.nanjundappa@alten.com>
2025-03-04 14:59:48 +0530

    funds api modified

--- backend/routes/fundRoutes.js
+++ backend/routes/fundRoutes.js
@@ -1,8 +1,8 @@
 const express = require("express");
-const { fetchFunds } = require("../controllers/fundController");
-const {authenticate }= require("../middleware/authMiddleware");
+const { fetchFunds, fetchFundFamilies } = require("../controllers/fundController");
+const { authenticate } = require("../middleware/authMiddleware");
 
 const router = express.Router();
 router.get("/fetchFunds", authenticate, fetchFunds);
-
+router.get("/fetchFundFamilies", fetchFundFamilies)
 module.exports = router;
